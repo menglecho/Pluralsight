6f1dc81474071b3f950d3d3798b5e43080ba4560
Meng Liang <mengl.echo@gmail.com>
2016-09-27 16:21:25 +0200

    SimpleClasses

---
+++ Transaction.cpp
@@ -0,0 +1,15 @@
+#include "Transaction.h"
+
+using namespace std;
+
+Transaction::Transaction(int amt, string kind) :amount(amt), type(kind){}
+
+string Transaction::Report() {
+	string report;
+	report += "   ";
+	report += type;
+	report += "   ";
+	report += to_string(amount);
+
+	return report;
+}
